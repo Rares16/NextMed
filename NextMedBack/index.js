86b663bac101718155ed84a29f56d540ad38c60a
retroarab <iosifdala@yahoo.com>
2024-11-08 15:42:14 +0200

    Defined the initial schema of the backend.

--- NextMedBack/index.js
+++ NextMedBack/index.js
@@ -1,11 +1,31 @@
-const express = require('express');
-const app = express();
-const PORT = 3000;
+require('dotenv').config(); // Import dotenv to read .env file
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+// Load the MongoDB URI from .env file
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
